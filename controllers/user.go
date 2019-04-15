213e21547f3eb1cd51753564c59d401636d5ae09
xiachang <14699454@qq.com>
2019-04-15 11:14:41 +0800

    add todo

--- controllers/user.go
+++ controllers/user.go
@@ -1,9 +1,9 @@
 package controllers
 
 import (
+	"../common/dto"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	"youtube-imitation-backstage/common/dto"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -12,24 +12,25 @@ type UserController struct {
 }
 
 func init() {
+	//todoxcs mysql 连接字符串放入配置文件中读取
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
 	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/youtubeImitation?charset=utf8")
 }
 
-func (c *UserController) Register(){
+func (c *UserController) Register() {
 	userName := c.GetString("userName")
 	email := c.GetString("email")
 	phoneNumber := c.GetString("phoneNumber")
 	password := c.GetString("password")
 	sex, _ := c.GetInt("sex")
 	o := orm.NewOrm()
-	beego.Info(email)
+	//todoxcs 修改成 beego 中 module 的方式
 	if userName != "" && email != "" && phoneNumber != "" && password != "" {
 		res, err := o.Raw("INSERT INTO `User`(`uid`, `userName`, `email`, `phoneNumber`, `password`, `sex`) VALUES (?, ?, ?, ?, ?, ?)", nil, userName, email, phoneNumber, password, sex).Exec()
 		if err == nil {
-			beego.Info(111111)
-			num, _:= res.RowsAffected()
+
+			num, _ := res.RowsAffected()
 			if num > 0 {
 				c.Data["json"] = dto.NewSuccessResponseDtoNilMsg("Register user success!!!")
 			} else {
@@ -39,7 +40,6 @@ func (c *UserController) Register(){
 		}
 	}
 }
-
 
 func (c *UserController) login() {
 
